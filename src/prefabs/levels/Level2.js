38e3f502462ec3705d69bc5332ff59d699181696
Kenny Doan <kennydoan110999@gmail.com>
2021-05-18 21:30:22 -0700

    Level Adjustment

--- src/prefabs/levels/Level2.js
+++ src/prefabs/levels/Level2.js
@@ -6,8 +6,8 @@ class Level2 extends BaseLevel{
     createLevel(){
         console.log("Level 2 Creation");
         this.createObjective(gameWidth/2 - 50, gameHeight/2, 'bum');
-        this.createObjective(gameWidth/2 - 350, gameHeight/2 + 100, 'bum');
-        this.createObjective(gameWidth/2 + 200, gameHeight/2 - 100, 'bum');
+        this.createObjective(gameWidth/2 - 350, gameHeight/2 - 150, 'bum');
+        this.createObjective(gameWidth/2 + 200, gameHeight/2 + 150, 'bum');
 
         this.createEnemy(
             null, 
